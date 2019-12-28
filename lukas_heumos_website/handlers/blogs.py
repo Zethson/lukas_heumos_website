868c70663a867e6accbdd8d330d6d6bfa3fc4693
Zethson <lukas.heumos@posteo.net>
2019-12-28 18:04:01 +0100

    [FEATURE] blog initialized - ready to write the first blogs!

--- lukas_heumos_website/handlers/blogs.py
+++ lukas_heumos_website/handlers/blogs.py
@@ -7,6 +7,6 @@ def blog_list():
     return render_template("blog_list.html")
 
 
-@app.route("/blog_article_1")
-def blog_article_1():
-    return render_template("blog_article_1.html")
+@app.route("/blog/spark_service")
+def spark_service():
+    return render_template("spark-service.html")
